c9005f8708f7309df4980a8ea1f234fe4d143add
ModulHor <30906839+ModulHor@users.noreply.github.com>
2017-08-15 16:07:40 -0500

    Crear clase HoraDia y HoraTipoHorario

---
+++ rua/rua-war/src/java/paq_horario/HoraDia.java
@@ -0,0 +1,62 @@
+
+package paq_horario;
+
+import framework.componentes.Division;
+import framework.componentes.PanelTabla;
+import framework.componentes.Tabla;
+import sistema.aplicacion.Pantalla;
+
+/**
+ *
+ * @author Andres
+ */
+public class HoraDia extends Pantalla {
+    private Tabla tab_hora_dia = new Tabla();
+    
+    public HoraDia(){
+    tab_hora_dia.setId("tab_hora_dia");   //identificador
+    tab_hora_dia.setTabla("yavirac_hora_dia", "id_yhodia", 1);
+    tab_hora_dia.dibujar();
+        /*agregarComponente(tab_hora_dia);*/ 
+        
+        PanelTabla pat_hora_dia = new PanelTabla();
+        pat_hora_dia.setId("pat_hora_dia");
+        pat_hora_dia.setPanelTabla(tab_hora_dia);
+        Division div_hora_dia = new Division();
+        div_hora_dia.setId("div_hora_dia");
+        div_hora_dia.dividir1(pat_hora_dia);
+        agregarComponente(div_hora_dia); 
+        
+    }
+
+    @Override
+    public void insertar() {
+        tab_hora_dia.insertar();
+    }
+
+    @Override
+    public void guardar() {
+        tab_hora_dia.guardar();
+        guardarPantalla();
+    }
+
+    @Override
+    public void eliminar() {
+       tab_hora_dia.eliminar();
+    }
+ 
+        
+
+
+    public Tabla getTab_hora_dia() {
+        return tab_hora_dia;
+    }
+
+    public void setTab_hora_dia(Tabla tab_hora_dia) {
+        this.tab_hora_dia = tab_hora_dia;
+    
+        
+        
+        
+                }
+}
