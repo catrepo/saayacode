79081e7d1544de0e687ab618a22a0d0abd7b22de
ModulHor <30906839+ModulHor@users.noreply.github.com>
2017-09-14 17:59:31 -0500

    Eliminar paquetes innecesarios de EJB

--- rua/rua-ejb/src/java/paq_horarios/ejb/ServiciosHorarios.java
+++ rua/rua-ejb/src/java/paq_horarios/ejb/ServiciosHorarios.java
@@ -27,4 +27,59 @@ public class ServiciosHorarios {
         return sql;
     }
     
+               /**
+     * Retorna la descripcion hora
+     *
+     * @param activo.- permite el ingreso del paramtero activo para filtrar ya sea true, false, o ambos.
+     * @return sql de la descripcion hora
+     */
+    public String getDescripcionHora(String codigo) {
+        String sql="";
+        sql="select  ide_yhodeh, c.ide_ystpea, d.ide_yhothj, e.ide_yhotih, a.ide_ystjor, f.ide_ystmod, hora_inicio_yhodeh, hora_final_yhodeh, activo_yhodeh, descripcion_ystjor, descripcion_ystpea, descripcion_yhothj, descripcion_yhotih, descripcion_ystmod \n" +
+"from yavirac_hora_definicion_hora a, yavirac_stror_jornada b, yavirac_stror_periodo_academic c, yavirac_hora_tipo_horario_jorna d, yavirac_hora_tipo_horario e, yavirac_stror_modalidad f\n" +
+"where a.ide_ystjor= b.ide_ystjor \n" +
+"and a.ide_ystpea= c.ide_ystpea \n" +
+"and a.ide_yhothj= d.ide_yhothj \n" +
+"and a.ide_yhotih= e.ide_yhotih \n" +
+"and a.ide_ystmod= f.ide_ystmod\n" +
+"and a.ide_ystpea = "+codigo+
+"order by descripcion_ystjor "
+                ;
+        return sql;
+    }
+     /**
+     * Retorna la hora
+     *
+     * @param activo.- permite el ingreso del paramtero activo para filtrar ya sea true, false, o ambos.
+     * @return sql de la tabla hora
+     */
+    public String getHora(String activo) {
+        String sql="";
+        sql="select ide_yhohor, descripcion_yhohor from yavirac_hora_hora  where activo_yhohor in ("+activo+") order by descripcion_yhohor desc";
+        return sql;
+    }
+    
+    /**
+     * Retorna el Tipo Horarios
+     *
+     * @param activo.- permite el ingreso del paramtero activo para filtrar ya sea true, false, o ambos.
+     * @return sql del tipo horarios
+     */
+    public String getTipoHorarios(String activo) {
+        String sql="";
+        sql="select ide_yhotih, descripcion_yhotih from yavirac_hora_tipo_horario  where activo_yhotih in ("+activo+") order by descripcion_yhotih ";
+        return sql;
+    }
+     /**
+     * Retorna el insert de la replica definicion hora
+     *
+     * @param activo.- permite el ingreso del paramtero activo para filtrar ya sea true, false, o ambos.
+     * @return sql del insert replica definicion hora
+     */
+    public String getInsertReplicaDefinicionHora(String insertar) {
+        String sql="";
+        sql="insert into yavirac_hora_definicion_hora (ide_yhodeh, ide_ystpea, ide_yhothj, ide_yhotih, ide_ystjor, ide_ystmod, hora_inicio_yhodeh, hora_final_yhodeh, activo_yhodeh)\n" +
+            "values () ";
+        return sql;
+    }
 }
