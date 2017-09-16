f26b15319d8cf225292cb69e0da9af13ceed18ba
nicolas-1994 <30878818+nicolas-1994@users.noreply.github.com>
2017-09-16 15:00:02 -0500

    Cree la pantalla asistencia

--- rua/rua-war/src/java/paq_asistencia/DiaAsistencia.java
+++ rua/rua-war/src/java/paq_asistencia/DiaAsistencia.java
@@ -19,50 +19,49 @@ import sistema.aplicacion.Utilitario;
  */
 public class DiaAsistencia extends Pantalla {
     
-    private Tabla tab_diaasistencia = new Tabla();
+    private Tabla tab_dia_asistencia = new Tabla();
  
    public DiaAsistencia() {
        
-       tab_diaasistencia.setId("tab_diaasistencia");
-       tab_diaasistencia.setTabla("yavirac_asis_dia_asistencia","ide_yasdas",1);
-       tab_diaasistencia.setHeader("DIA ASISTENCIA");
-       tab_diaasistencia.dibujar();
+       tab_dia_asistencia.setId("tab_diaasistencia");
+       tab_dia_asistencia.setTabla("yavirac_asis_dia_asistencia","ide_yasdas",1);
+       tab_dia_asistencia.setHeader("DIA ASISTENCIA");
+       tab_dia_asistencia.dibujar();
         
         
-        PanelTabla pat_diaasistencia = new PanelTabla();
-        pat_diaasistencia.setId("pat_diaasistencia");
-        pat_diaasistencia.setPanelTabla(tab_diaasistencia);
+        PanelTabla pat_dia_asistencia = new PanelTabla();
+        pat_dia_asistencia.setId("pat_diaasistencia");
+        pat_dia_asistencia.setPanelTabla(tab_dia_asistencia);
        
-        Division div_diaasistencia = new Division();
-        div_diaasistencia.setId("div_diaasistencia");
-        div_diaasistencia.dividir1(pat_diaasistencia);
-        
-        agregarComponente(div_diaasistencia);
+        Division div_dia_asistencia = new Division();
+        div_dia_asistencia.setId("div_dia_asistencia");
+        div_dia_asistencia.dividir1(pat_dia_asistencia);
+        agregarComponente(div_dia_asistencia);
     
       } 
     @Override
     public void insertar() {
-        tab_diaasistencia.insertar();
+        tab_dia_asistencia.insertar();
     }
 
     @Override
     public void guardar() {
-        tab_diaasistencia.guardar();
+        tab_dia_asistencia.guardar();
         guardarPantalla();
         
     }
 
     @Override
     public void eliminar() {
-        tab_diaasistencia.eliminar();
+        tab_dia_asistencia.eliminar();
     }
 
     public Tabla getTab_diaasistencia() {
-        return tab_diaasistencia;
+        return tab_dia_asistencia;
     }
 
-    public void setTab_diaasistencia(Tabla tab_diaasistencia) {
-        this.tab_diaasistencia = tab_diaasistencia;
+    public void setTab_diaasistencia(Tabla tab_dia_asistencia) {
+        this.tab_dia_asistencia = tab_dia_asistencia;
     }
 
 }
