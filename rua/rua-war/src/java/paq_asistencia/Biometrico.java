8fd828fef81d7eba85e954fd21cdd002a546fe2f
nicolas-1994 <30878818+nicolas-1994@users.noreply.github.com>
2017-09-16 15:45:30 -0500

    Cree la pantalla asistencia

--- rua/rua-war/src/java/paq_asistencia/Biometrico.java
+++ rua/rua-war/src/java/paq_asistencia/Biometrico.java
@@ -10,6 +10,7 @@ import framework.componentes.Division;
 import framework.componentes.Grupo;
 import framework.componentes.PanelTabla;
 import framework.componentes.Tabla;
+import framework.componentes.Upload;
 import sistema.aplicacion.Pantalla;
 import sistema.aplicacion.Utilitario;
 
@@ -20,8 +21,13 @@ import sistema.aplicacion.Utilitario;
 public class Biometrico extends Pantalla {
     
     private Tabla tab_biometrico = new Tabla();
- 
+    private Upload upl_archivo=new Upload();
+
    public Biometrico() {
+       
+       bar_botones.getBot_guardar().setRendered(false);
+       bar_botones.getBot_eliminar().setRendered(false);
+       bar_botones.getBot_insertar().setRendered(false);
        
        tab_biometrico.setId("tab_biometrico");
         tab_biometrico.setTabla("yavirac_asis_biometrico","ide_yasbio",1);
@@ -38,7 +44,16 @@ public class Biometrico extends Pantalla {
         div_biometrico.dividir1(pat_biometrico);
         
         agregarComponente(div_biometrico);
-    
+        
+        upl_archivo.setId("upl_archivo");
+		upl_archivo.setMetodo("validarArchivo");
+
+		upl_archivo.setUpdate("gri_valida");		
+		upl_archivo.setAuto(false);
+		upl_archivo.setAllowTypes("/(\\.|\\/)(xls)$/");
+		upl_archivo.setUploadLabel("Validar");
+		upl_archivo.setCancelLabel("Cancelar Seleccion");
+         bar_botones.agregarComponente(upl_archivo);
       } 
     @Override
     public void insertar() {
@@ -65,4 +80,13 @@ public class Biometrico extends Pantalla {
         this.tab_biometrico = tab_biometrico;
     }
 
+    public Upload getUpl_archivo() {
+        return upl_archivo;
+    }
+
+    public void setUpl_archivo(Upload upl_archivo) {
+        this.upl_archivo = upl_archivo;
+    }
+
+    
 }
