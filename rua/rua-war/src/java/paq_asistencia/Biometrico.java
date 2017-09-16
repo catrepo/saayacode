943a90656e720de17498633bee8c332b8d10f169
nicolas-1994 <30878818+nicolas-1994@users.noreply.github.com>
2017-09-16 16:00:05 -0500

    Cree la pantalla asistencia

--- rua/rua-war/src/java/paq_asistencia/Biometrico.java
+++ rua/rua-war/src/java/paq_asistencia/Biometrico.java
@@ -6,6 +6,8 @@
 package paq_asistencia;
 
 import framework.componentes.Barra;
+import framework.componentes.Boton;
+import framework.componentes.Dialogo;
 import framework.componentes.Division;
 import framework.componentes.Grupo;
 import framework.componentes.PanelTabla;
@@ -22,12 +24,24 @@ public class Biometrico extends Pantalla {
     
     private Tabla tab_biometrico = new Tabla();
     private Upload upl_archivo=new Upload();
+    private Dialogo dia_subir_archivo = new Dialogo();
+            
 
    public Biometrico() {
        
        bar_botones.getBot_guardar().setRendered(false);
        bar_botones.getBot_eliminar().setRendered(false);
        bar_botones.getBot_insertar().setRendered(false);
+       
+       Boton bot_abrir_upload = new Boton();
+       bot_abrir_upload.setValue("Subir Marcaciones");
+       bot_abrir_upload.setMetodo("abrirUpload");
+       bar_botones.agregarBoton(bot_abrir_upload);
+       
+       dia_subir_archivo.setId("dia_subir_archivo");
+       dia_subir_archivo.setTitle("Subir Marcaciones Biometrico");
+       dia_subir_archivo.getBot_aceptar().setRendered(false);
+       agregarComponente(dia_subir_archivo);
        
        tab_biometrico.setId("tab_biometrico");
         tab_biometrico.setTabla("yavirac_asis_biometrico","ide_yasbio",1);
@@ -48,13 +62,16 @@ public class Biometrico extends Pantalla {
         upl_archivo.setId("upl_archivo");
 		upl_archivo.setMetodo("validarArchivo");
 
-		upl_archivo.setUpdate("gri_valida");		
+		//upl_archivo.setUpdate("gri_valida");	//	
 		upl_archivo.setAuto(false);
 		upl_archivo.setAllowTypes("/(\\.|\\/)(xls)$/");
 		upl_archivo.setUploadLabel("Validar");
 		upl_archivo.setCancelLabel("Cancelar Seleccion");
          bar_botones.agregarComponente(upl_archivo);
       } 
+   public void abrirUpload(){
+       dia_subir_archivo.dibujar();
+   }
     @Override
     public void insertar() {
         tab_biometrico.insertar();
