da13dc6e5bf2dd564009d49a0f95adfe562af0aa
Minta96 <30877963+Minta96@users.noreply.github.com>
2017-09-20 15:45:01 -0500

    programacion de botones

--- rua/rua-war/src/java/paq_estructura/PeriodoAcademico.java
+++ rua/rua-war/src/java/paq_estructura/PeriodoAcademico.java
@@ -7,6 +7,7 @@ package paq_estructura;
 import framework.componentes.Division;
 import framework.componentes.PanelTabla;
 import framework.componentes.Tabla;
+import framework.componentes.Tabulador;
 import sistema.aplicacion.Pantalla;
 /**
  *
@@ -15,24 +16,63 @@ import sistema.aplicacion.Pantalla;
 public class PeriodoAcademico extends Pantalla{
    
     Tabla tab_periodo_academic = new Tabla (); // importar tabla 
+    Tabla tab_periodo_evaluacion= new Tabla();
+    Tabla tab_actividad_evaluacion=new Tabla();
     
     public PeriodoAcademico (){
            
         
-        tab_periodo_academic.setId("tab_periodo_academico");  // todo objeto instanciado poner id 
         
-        tab_periodo_academic.setTabla("yavirac_stror_periodo_academic","ide_ytiace",1);    // nom bdd
+        tab_periodo_academic.setId("tab_periodo_academic");  // todo objeto instanciado poner id 
+        
+        tab_periodo_academic.setTabla("yavirac_stror_periodo_academic","ide_ystpea",1);    // nom bdd
+        tab_periodo_academic.agregarRelacion(tab_periodo_evaluacion);
+        tab_periodo_academic.agregarRelacion(tab_periodo_evaluacion);
+
         tab_periodo_academic.dibujar();
         
         PanelTabla pa_periodoacademico = new PanelTabla();
         pa_periodoacademico.setId("pa_periodoacademico"); // nombre de i
         pa_periodoacademico.setPanelTabla(tab_periodo_academic);
         
-        Division div_periodoacademico = new Division();
-        div_periodoacademico.setId("div_periodoacademico");
-        div_periodoacademico.dividir1(pa_periodoacademico);
+   
         
-        agregarComponente(div_periodoacademico);
+        //tabla perioado de evaluacion
+        
+        tab_periodo_evaluacion.setId("tab_periodo_evaluacion");  // todo objeto instanciado poner id 
+        
+        tab_periodo_evaluacion.setTabla("yavirac_nota_periodo_evaluacio","ide_ynopee",1);    // nom bdd
+        //tab_periodo_evaluacion.getColumna("")
+        tab_periodo_evaluacion.dibujar();
+        
+        PanelTabla pa_periodoevaluacion = new PanelTabla();
+        pa_periodoevaluacion.setId("pa_periodoevaluacion"); // nombre de i
+        pa_periodoevaluacion.setPanelTabla(tab_periodo_evaluacion);
+        
+        
+        //tabala  periodo actividad de evaluacion
+        
+        tab_actividad_evaluacion.setId("tab_actividad_evaluacion");  // todo objeto instanciado poner id 
+        
+        tab_actividad_evaluacion.setTabla("yavirac_nota_periodo_activ_eva","ide_ynopae",1);    // nom bdd
+        tab_actividad_evaluacion.dibujar();
+        
+        PanelTabla pa_actividadevaluacion = new PanelTabla();
+        pa_actividadevaluacion.setId("pa_actividadevaluacion"); // nombre de i
+        pa_actividadevaluacion.setPanelTabla(tab_actividad_evaluacion);
+        
+
+        ///// tabuladores
+		Tabulador tab_tabulador=new Tabulador();
+		tab_tabulador.setId("tab_tabulador");
+
+		tab_tabulador.agregarTab("PERIODO EVALUACION", pa_periodoevaluacion);
+		tab_tabulador.agregarTab("PERIODO ACTIVIDAD EVALUACION", pa_actividadevaluacion);
+
+		Division div_periodo_academico =new Division();
+		div_periodo_academico.dividir2(pa_periodoacademico, tab_tabulador, "30%", "h");
+		agregarComponente(div_periodo_academico);
+        
     }
 
     @Override
@@ -59,9 +99,26 @@ public class PeriodoAcademico extends Pantalla{
     public void setTab_periodo_academic(Tabla tab_periodo_academic) {
         this.tab_periodo_academic = tab_periodo_academic;
     }
+
+    public Tabla getTab_periodo_evaluacion() {
+        return tab_periodo_evaluacion;
+    }
+
+    public void setTab_periodo_evaluacion(Tabla tab_periodo_evaluacion) {
+        this.tab_periodo_evaluacion = tab_periodo_evaluacion;
+    }
+
+    public Tabla getTab_actividad_evaluacion() {
+        return tab_actividad_evaluacion;
+    }
+
+    public void setTab_actividad_evaluacion(Tabla tab_actividad_evaluacion) {
+        this.tab_actividad_evaluacion = tab_actividad_evaluacion;
+    }
     
     
     }
+
     
     
 
