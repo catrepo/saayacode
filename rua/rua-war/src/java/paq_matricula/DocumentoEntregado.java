0e5343efb02773f924e550fff7741ad347f5ac79
ALEXASN <30878000+ALEXASN@users.noreply.github.com>
2017-10-09 14:35:24 -0500

    PROGRAMACION

--- rua/rua-war/src/java/paq_matricula/DocumentoEntregado.java
+++ rua/rua-war/src/java/paq_matricula/DocumentoEntregado.java
@@ -13,20 +13,20 @@ public class DocumentoEntregado extends Pantalla{
         
      public DocumentoEntregado() {//constructor
        tab_documento_entregado.setId("tab_documento_entregado");// todo objeto instanciado poner id 
-        tab_documento_entregado.setTabla("public.yavirac_matri_docu_entre_matri", "ide_ymdem", 1);  // nombre de la base de datos ii la clave primaria
+        tab_documento_entregado.setTabla("yavirac_matri_docu_entre_matri", "ide_ymdem", 1);  // nombre de la base de datos ii la clave primaria
         tab_documento_entregado.setHeader("DOCUMENTOS ENTREGADOS");
         tab_documento_entregado.dibujar();//dibuja la tabla
 
-        PanelTabla pa_actividad_evaluacion = new PanelTabla();//intanciamos el panel del framework
-        pa_actividad_evaluacion.setId("pa_documento_entregado");//nombre id
-        pa_actividad_evaluacion.setPanelTabla(tab_documento_entregado);//agregar a nuestra tabla el panel
+        PanelTabla pa_documento_entregado = new PanelTabla();//intanciamos el panel del framework
+        pa_documento_entregado.setId("pa_documento_entregado");//nombre id
+        pa_documento_entregado.setPanelTabla(tab_documento_entregado);//agregar a nuestra tabla el panel
         
         //instanciar una division del framework
-        Division div_actividad_evaluacion =new Division ();//instanciamos
-        div_actividad_evaluacion.setId("div_documento_entregado");//es un idientificador
-        div_actividad_evaluacion.dividir1(tab_documento_entregado);
+        Division div_documento_entregado =new Division ();//instanciamos
+        div_documento_entregado.setId("div_documento_entregado");//es un idientificador
+        div_documento_entregado.dividir1(tab_documento_entregado);
         
-        agregarComponente(div_actividad_evaluacion);//agregar componente
+        agregarComponente(div_documento_entregado);//agregar componente
     }
     @Override
     public void insertar() {
--- rua/rua-war/src/java/paq_matricula/RegistroCredito.java
+++ rua/rua-war/src/java/paq_matricula/RegistroCredito.java
@@ -1,14 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package paq_matricula;
 
-/**
- *
- * @author PC
- */
-public class RegistroCredito {
+import framework.componentes.Division;
+import framework.componentes.PanelTabla;
+import framework.componentes.Tabla;
+import sistema.aplicacion.Pantalla;
+
+
+public class RegistroCredito extends Pantalla{
     
+        private Tabla tab_registro_credito = new Tabla();//instanciar tabla del framework
+        
+     public RegistroCredito() {//constructor
+       tab_registro_credito.setId("tab_documento_entregado");// todo objeto instanciado poner id 
+      //  tab_registro_credito.setTabla("yavirac_matri_docu_entre_matri", "ide_ymdem", 1);  // ponombre de la base de datos ii la clave primaria
+        tab_registro_credito.setHeader("REGISTRO DE CREDITOS");
+        tab_registro_credito.dibujar();//dibuja la tabla
+
+        PanelTabla pa_registro_credito = new PanelTabla();//intanciamos el panel del framework
+       pa_registro_credito.setId("pa_registro_credito");//nombre id
+        pa_registro_credito.setPanelTabla(tab_registro_credito);//agregar a nuestra tabla el panel
+        
+        //instanciar una division del framework
+        Division div_registro_credito =new Division ();//instanciamos
+        div_registro_credito.setId("div_documento_entregado");//es un idientificador
+       div_registro_credito.dividir1(tab_registro_credito);
+        
+        agregarComponente(div_registro_credito);//agregar componente
+    }
+    @Override
+    public void insertar() {
+       tab_registro_credito.insertar();
+    }
+
+    @Override
+    public void guardar() {
+        tab_registro_credito.guardar();
+        guardarPantalla();
+    }
+
+    @Override
+    public void eliminar() {
+        tab_registro_credito.eliminar();
+    }
+    
+    //generar geter and seter
+    public Tabla getTab_actividad_evaluacion() {
+        return tab_registro_credito;
+    }
+
+    public void setTab_actividad_evaluacion(Tabla  tab_registro_credito) {
+        this.tab_registro_credito = tab_registro_credito;
+    }
 }
