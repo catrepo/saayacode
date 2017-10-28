a7627528173b4c194e8c54bcc88c09f042db3512
ALEXASN <30878000+ALEXASN@users.noreply.github.com>
2017-10-28 12:27:03 -0500

    programacion

--- rua/rua-war/src/java/paq_matricula/RegistroCredito.java
+++ rua/rua-war/src/java/paq_matricula/RegistroCredito.java
@@ -15,21 +15,44 @@ public class RegistroCredito extends Pantalla{
         private Tabla tab_registro_credito = new Tabla();//instanciar tabla del framework
         
      public RegistroCredito() {//constructor
-       tab_registro_credito.setId("tab_registro_credito");// todo objeto instanciado poner id 
-      //  tab_registro_credito.setTabla("yavirac_matri_docu_entre_matri", "ide_ymdem", 1);  // ponombre de la base de datos ii la clave primaria
-        tab_registro_credito.setHeader("REGISTRO DE CREDITOS");
-        tab_registro_credito.dibujar();//dibuja la tabla
+        tab_registro_credito.setId("tab_registro_credito");// todo objeto instanciado poner id 
+       tab_registro_credito.setTabla("yavirac_matri_tipo_reg_cred", "ide_ymatrc", 1);  // nombre de la base de datos ii la clave primaria
+       tab_registro_credito.setHeader("REGISTRO DE CREDITOS");
+       tab_registro_credito.dibujar();//dibuja la tabla
 
         PanelTabla pa_registro_credito = new PanelTabla();//intanciamos el panel del framework
-       pa_registro_credito.setId("pa_registro_credito");//nombre id
+        pa_registro_credito.setId("pa_documento_entregado");//nombre id
         pa_registro_credito.setPanelTabla(tab_registro_credito);//agregar a nuestra tabla el panel
         
         //instanciar una division del framework
         Division div_registro_credito =new Division ();//instanciamos
-        div_registro_credito.setId("div_documento_entregado");//es un idientificador
-       div_registro_credito.dividir1(tab_registro_credito);
+       div_registro_credito.setId("div_documento_entregado");//es un idientificador
+        div_registro_credito.dividir1(tab_registro_credito);
         
         agregarComponente(div_registro_credito);//agregar componente
+    }
+    @Override
+    public void insertar() {
+        tab_registro_credito.insertar();
+    }
+
+    @Override
+    public void guardar() {
+       tab_registro_credito.guardar();
+        guardarPantalla();
+    }
+
+    @Override
+    public void eliminar() {
+        tab_registro_credito.eliminar();
+    }
+
+    public Tabla getTab_documento_entregado() {
+        return tab_registro_credito;
+    }
+
+    public void setTab_documento_entregado(Tabla tab_registro_credito) {
+        this.tab_registro_credito = tab_registro_credito;
     }
 
     public Utilitario getUtilitario() {
@@ -55,28 +78,6 @@ public class RegistroCredito extends Pantalla{
     public void setGru_pantalla(Grupo gru_pantalla) {
         this.gru_pantalla = gru_pantalla;
     }
-    @Override
-    public void insertar() {
-       tab_registro_credito.insertar();
-    }
-
-    @Override
-    public void guardar() {
-        tab_registro_credito.guardar();
-        guardarPantalla();
-    }
-
-    @Override
-    public void eliminar() {
-        tab_registro_credito.eliminar();
-    }
     
-    //generar geter and seter
-    public Tabla getTab_registro_credito() {
-        return tab_registro_credito;
-    }
-
-    public void setTab_actividad_evaluacion(Tabla  tab_registro_credito) {
-        this.tab_registro_credito = tab_registro_credito;
-    }
+    
 }
