677eaa68d39d5818d8df8007973164ae9cb51ff0
Kelly Collahuazo <30843517+kellycollahuazo@users.noreply.github.com>
2017-10-30 12:58:56 -0500

    modificacion

--- rua/rua-ejb/src/java/paq_alumno/ejb/ServicioAlumno.java
+++ rua/rua-ejb/src/java/paq_alumno/ejb/ServicioAlumno.java
@@ -28,6 +28,30 @@ public class ServicioAlumno {
         sql="Select ide_yaldap,nombre_yaldap,apellido_yaldap,doc_identidad_yaldap FROM yavirac_alum_dato_personal where activo_yaldap in ("+estado+") order by nombre_yaldap";
         return sql;
     }
-    
+       /**
+     * Consultamos los datos de la vivienda
+     *
+     * @return los datos de la vivienda
+     */
+    public String getDatosVivienda(String estado) {
+        String sql="";
+        sql="SELECT ide_yaltiv, descripcion_yaltip FROM yavirac_alum_tipo_vivienda";
+        return sql;
+    }
+      /**
+     * Consultamos los datos de quien cubre gastos
+     *
+     * @return los datos de quien cubre los gastos
+     */
+    public String getGasto(String estado) {
+        String sql="";
+        sql="SELECT ide_yalgas, descripcion_yalgas FROM yavirac_alum_gasto";
+        return sql;
+    } 
+    public String getOcupacion(String activo) {
+        String sql="";
+        sql="SELECT ide_yalocu, descripcion_yalocu FROM yavirac_alum_ocupacion where activo_yalocu in ("+activo+")";
+        return sql;
+    } 
     
 }
