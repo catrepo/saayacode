4dc65a6f61eb490faa3a72e36b48b3467b44cc99
nicolas-1994 <30878818+nicolas-1994@users.noreply.github.com>
2018-04-14 17:05:20 -0500

    Actualizado Asistencia Biometrico

--- rua/rua-war/src/java/paq_asistencia/Biometrico.java
+++ rua/rua-war/src/java/paq_asistencia/Biometrico.java
@@ -126,7 +126,7 @@ public class Biometrico extends Pantalla {
         tab_biometrico.getColumna("nombre_yasbio").setLectura(true);
         tab_biometrico.getColumna("departamento_yasbio").setLectura(true);
         tab_biometrico.getColumna("fecha_registro_yasbio").setLectura(true);
-        tab_biometrico.getColumna("codigo_reloj_yasbio").setLectura(false);
+        tab_biometrico.getColumna("codigo_reloj_yasbio").setLectura(true);
         
         tab_biometrico.dibujar();
         //tab_biometrico.setRows(10);
