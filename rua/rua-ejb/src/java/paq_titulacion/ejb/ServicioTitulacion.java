1fac059da6de25eaca61829537e20ea5ca2db6a5
beatrizaltamirano <30906975+beatrizaltamirano@users.noreply.github.com>
2018-04-14 17:56:48 -0500

    actualizacion de la clase libreta de practicas

--- rua/rua-ejb/src/java/paq_titulacion/ejb/ServicioTitulacion.java
+++ rua/rua-ejb/src/java/paq_titulacion/ejb/ServicioTitulacion.java
@@ -75,7 +75,7 @@ public class ServicioTitulacion {
 public String getDatoEmpresa() {
           
         String sql="";
-        sql="select ide_ytiemp,ruc_ytiemp from yavirac_titu_empresa";
+        sql="select ide_ytiemp, nombre_comercial_ytiemp,ruc_ytiemp from yavirac_titu_empresa";
         return sql;
 }
 
