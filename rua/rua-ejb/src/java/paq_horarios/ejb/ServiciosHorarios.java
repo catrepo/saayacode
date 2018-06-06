8c88790c7041eda19c97283df724fb757bc9a905
catrepo <30820089+catrepo@users.noreply.github.com>
2018-06-06 01:35:14 -0500

    commit horarios

--- rua/rua-ejb/src/java/paq_horarios/ejb/ServiciosHorarios.java
+++ rua/rua-ejb/src/java/paq_horarios/ejb/ServiciosHorarios.java
@@ -207,7 +207,40 @@ public class ServiciosHorarios {
         public String getResultadoExisteReceso(String hora_receso,String hora_inicio,String hora_fin) {
         String sql="";
         sql="select 1 as resultado,2 as respuesta from yavirac_hora_definicion_hora where ('"+hora_receso+"'::interval + '60' ::interval) between '"+hora_inicio+"'::interval and '"+hora_fin+"'::interval limit 1" ;
-        System.out.println("imprimo resultao receso "+sql);
+        //System.out.println("imprimo resultao receso "+sql);
         return sql;
-         }        
+         }  
+        
+        public String insertHorarioMatriz(String tipo_horario,String periodo) {
+        String sql="";
+        sql="INSERT INTO yavirac_matriz_temp(ide_yamatz, ide_ystmal, ide_ystnie,ide_ystmat,ide_ystmen,ide_ysttif,ide_ystjor,ide_ymacal, ide_yhogra, ide_ystins, ide_ypedpe,   " +
+            "            numero_horas_ystmal, prioridad_materia_ystmal, maximo_horas_ystmal,ide_ystpea,minimo_horas_ystmal, aplica_laboratorio_ystmal, num_min_lab_ystmal, " +
+            "            num_max_lab_ystmal,horas_semana_ystmal,horas_semana_lab_ystmal)" +
+            " select row_number()over(order by a.ide_ystmal) + (select (case when max( ide_yamatz) is null then 0 else max( ide_yamatz) end) as codigo from yavirac_matriz_temp) as ide, " +
+            " a.ide_ystmal, a.ide_ystnie, a.ide_ystmat, a.ide_ystmen, a.ide_ysttif,a.ide_ystjor,a.ide_ymacal,a.ide_yhogra, b.ide_ystins,c.ide_ypedpe," +
+            " a.numero_horas_ystmal, a.prioridad_materia_ystmal, a.maximo_horas_ystmal, a.ide_ystpea," +
+            " a.minimo_horas_ystmal, a.aplica_laboratorio_ystmal, a.num_min_lab_ystmal, a.num_max_lab_ystmal,a.horas_semana_ystmal,a.horas_semana_lab_ystmal" +
+            " from (" +
+            " select a.ide_ystmal, a.ide_ystnie, a.ide_ystmat, a.ide_ystmen, a.ide_ysttif, a.numero_horas_ystmal, a.prioridad_materia_ystmal, a.maximo_horas_ystmal, " +
+            " a.minimo_horas_ystmal, a.aplica_laboratorio_ystmal, a.num_min_lab_ystmal, a.num_max_lab_ystmal,ide_ystjor,ide_yhogra,ide_ymacal,b.ide_ystpea,a.horas_semana_ystmal,a.horas_semana_lab_ystmal" +
+            " from yavirac_stror_malla a,yavirac_stror_periodo_mension b,  yavirac_hora_malla_periodo c,yavirac_matri_cupo_alumno d" +
+            " where  a.ide_ystmen = b.ide_ystmen" +
+            " and b.ide_ystpea= c.ide_ystpea" +
+            " and a.ide_ystmal= c.ide_ystmal" +
+            " and b.ide_ystpea=d.ide_ystpea" +
+            " and a.ide_ystnie=d.ide_ystnie" +
+            " and a.ide_ystmen=d.ide_ystmen" +
+            " and b.ide_ystpea =" +periodo+
+            " ) a" +
+            " left join yavirac_hora_aula_grupo b on a.ide_ymacal=b.ide_ymacal" +
+            " left join yavirac_perso_malla_docente c on a.ide_ystmal=c.ide_ystmal and a.ide_ystpea= c.ide_ystpea and a.ide_yhogra=c.ide_yhogra and a.ide_ystjor = c.ide_ystjor" ;
+            if(tipo_horario.equals("1")){
+            sql +=" where aplica_laboratorio_ystmal=true" ;
+            }
+            if(tipo_horario.equals("2")){
+            sql +=" where aplica_laboratorio_ystmal=false" ;
+            }
+        //System.out.println("imprimo resultao receso "+sql);
+        return sql;
+         }  
 }
