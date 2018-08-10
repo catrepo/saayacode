9a1b3104dd309228e16154b2c86f4f781ccd72de
guacho20 <30877975+guacho20@users.noreply.github.com>
2018-08-10 12:54:12 -0500

    correccion notas

--- rua/rua-war/src/java/paq_nota/Nota.java
+++ rua/rua-war/src/java/paq_nota/Nota.java
@@ -6,16 +6,17 @@
 package paq_nota;
 
 import framework.aplicacion.TablaGenerica;
-import framework.componentes.Boton;
 import framework.componentes.Combo;
-import framework.componentes.Division;
 import framework.componentes.Etiqueta;
+import framework.componentes.Grid;
 import framework.componentes.Grupo;
-import framework.componentes.PanelTabla;
 import framework.componentes.Tabla;
 import java.util.List;
 import javax.ejb.EJB;
+import paq_alumno.ejb.ServicioAlumno;
+import paq_asistencia.ejb.ServicioAsistencia;
 import paq_estructura.ejb.ServicioEstructuraOrganizacional;
+import paq_matricula.ejb.ServicioMatriculas;
 import paq_personal.ejb.ServicioPersonal;
 import sistema.aplicacion.Pantalla;
 
@@ -26,58 +27,55 @@ import sistema.aplicacion.Pantalla;
 public class Nota extends Pantalla {
 
     private Combo com_periodo_academico = new Combo();
+    private Combo com_materia_docente = new Combo();
     private Tabla tab_docente_mencion = new Tabla();
-    private Tabla tab_tipo_evaluacion = new Tabla();
-
+    private Etiqueta eti_docente = new Etiqueta();
+    private Etiqueta eti_materia = new Etiqueta();
+    
     @EJB
     private final ServicioEstructuraOrganizacional ser_estructura_organizacional = (ServicioEstructuraOrganizacional) utilitario.instanciarEJB(ServicioEstructuraOrganizacional.class);
     @EJB
     private final ServicioPersonal ser_personal = (ServicioPersonal) utilitario.instanciarEJB(ServicioPersonal.class);
-
+     @EJB
+    private final ServicioMatriculas ser_matricula = (ServicioMatriculas) utilitario.instanciarEJB(ServicioMatriculas.class);
+    @EJB
+    private final ServicioAlumno ser_alumno = (ServicioAlumno) utilitario.instanciarEJB(ServicioAlumno.class);
+    @EJB
+    private final ServicioAsistencia ser_asistencia = (ServicioAsistencia) utilitario.instanciarEJB(ServicioAsistencia.class);
+   
     public Nota() {
         if (TienePerfilNota()) {
 
             bar_botones.getBot_insertar().setRendered(false);
-            bar_botones.getBot_guardar().setRendered(false);
             bar_botones.getBot_eliminar().setRendered(false);
             bar_botones.getBot_atras().setRendered(false);
             bar_botones.getBot_fin().setRendered(false);
             bar_botones.getBot_siguiente().setRendered(false);
             bar_botones.getBot_inicio().setRendered(false);
 
-            bar_botones.agregarComponente(new Etiqueta("Docente *****"));
-            bar_botones.agregarComponente(new Etiqueta(docente));
-            bar_botones.agregarComponente(new Etiqueta("*****"));
-            bar_botones.agregarComponente(new Etiqueta("      "));
-
-            /*Boton bot_asistencia = new Boton();
-            bot_asistencia.setValue("Registrar Asistencia");
-            bot_asistencia.setMetodo("registrarAsistencia");
-            bar_botones.agregarBoton(bot_asistencia);*/
-
             com_periodo_academico.setId("com_periodo_academico");
             com_periodo_academico.setCombo(ser_estructura_organizacional.getPeriodoAcademico("true"));
             bar_botones.agregarComponente(new Etiqueta("Periodo Academico"));
             bar_botones.agregarComponente(com_periodo_academico);
             com_periodo_academico.setMetodo("filtroComboPeriodoAcademico");
-            /*tab_tipo_evaluacion.setId("tab_tipo_evaluacion");
-            tab_tipo_evaluacion.setTabla("yavirac_nota_tipo_evaluacion", "ide_ynotie", 1);
-            tab_tipo_evaluacion.setHeader("TIPO DE EVALUACIÓN");
-            tab_tipo_evaluacion.getColumna("ide_ynotie").setNombreVisual("CODIGO");
-            tab_tipo_evaluacion.getColumna("descripcion_ynotie").setNombreVisual("DESCRIPCION");
-            tab_tipo_evaluacion.getColumna("activo_ynotie").setNombreVisual("ACTIVO");
-            tab_tipo_evaluacion.dibujar();
 
-            PanelTabla pat_tipo_evaluacion = new PanelTabla();
-            pat_tipo_evaluacion.setId("pat_tipo_evaluacion");
-            pat_tipo_evaluacion.setPanelTabla(tab_tipo_evaluacion);
+            com_materia_docente.setId("com_materia_docente");
+           // com_materia_docente.setCombo(ser_asistencia.getMateriaNivelDocente("-1", "2"));
 
-            Division div_tipo_evaluacion = new Division();
-            div_tipo_evaluacion.setId("div_tipo_evaluacion");
-            div_tipo_evaluacion.dividir1(pat_tipo_evaluacion);
+            bar_botones.agregarComponente(new Etiqueta("Cursos:"));
+            bar_botones.agregarComponente(com_materia_docente);
+            //com_materia_docente.setMetodo("filtroHorarios");
+            // boton limpiar
 
-            agregarComponente(div_tipo_evaluacion);*/
-
+            eti_docente.setStyle("font-size: 16px;font-weight: bold");
+            eti_docente.setValue("Docente: " + docente);
+            Grid grup_titulo = new Grid();
+            grup_titulo.setColumns(1);
+            grup_titulo.setWidth("100%");
+            grup_titulo.setId("grup_titulo");
+            grup_titulo.getChildren().add(eti_docente);
+            grup_titulo.getChildren().add(new Etiqueta(""));
+           
         } else {
             utilitario.agregarNotificacionInfo("Mensaje", "EL usuario ingresado no registra permisos para el control de Asistencia. Consulte con el Administrador");
         }
@@ -107,6 +105,17 @@ public class Nota extends Pantalla {
         }
     }
 
+    public void filtraAlumno() {
+        if (com_periodo_academico.getValue() == null) {
+            utilitario.agregarMensajeInfo("Adevertencia: ", "Seleccione el Periodo Académico");
+            return;
+        }
+        if (com_materia_docente.getValue() == null) {
+            utilitario.agregarMensajeInfo("Adevertencia: ", "Seleccione la materia que desea consultar la asistencia");
+            return;
+        }
+    }
+
     public void filtraEstudiantes() {
         String malla = tab_docente_mencion.getValorSeleccionado();
         TablaGenerica tab_malla = utilitario.consultar("select ide_ypemad,ide_ystmal,ide_ypedpe from yavirac_perso_malla_docente where ide_ypemad=" + malla);
@@ -115,11 +124,8 @@ public class Nota extends Pantalla {
 
     public void filtroComboPeriodoAcademico() {
 
-        tab_docente_mencion.setCondicion("ide_ystpea=" + com_periodo_academico.getValue().toString() + " and ide_ypedpe =" + ide_docente);
-        tab_docente_mencion.ejecutarSql();
-        //tab_fecha_control.setCondicion("ide_ystpea=" + com_periodo_academico.getValue().toString());
-        //tab_fecha_control.ejecutarSql();
-        utilitario.addUpdate("tab_docente_mencion");
+        com_materia_docente.setCombo(ser_asistencia.getMateriaNivelDocente(com_periodo_academico.getValue().toString(), ide_docente));
+        utilitario.addUpdate("com_materia_docente");
 
     }
 
@@ -138,46 +144,6 @@ public class Nota extends Pantalla {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public Combo getCom_periodo_academico() {
-        return com_periodo_academico;
-    }
-
-    public void setCom_periodo_academico(Combo com_periodo_academico) {
-        this.com_periodo_academico = com_periodo_academico;
-    }
-
-    public String getDocente() {
-        return docente;
-    }
-
-    public void setDocente(String docente) {
-        this.docente = docente;
-    }
-
-    public String getDocumento() {
-        return documento;
-    }
-
-    public void setDocumento(String documento) {
-        this.documento = documento;
-    }
-
-    public String getIde_docente() {
-        return ide_docente;
-    }
-
-    public void setIde_docente(String ide_docente) {
-        this.ide_docente = ide_docente;
-    }
-
-    public Grupo getGru_pantalla() {
-        return gru_pantalla;
-    }
-
-    public void setGru_pantalla(Grupo gru_pantalla) {
-        this.gru_pantalla = gru_pantalla;
-    }
-
     public Tabla getTab_docente_mencion() {
         return tab_docente_mencion;
     }
@@ -186,12 +152,20 @@ public class Nota extends Pantalla {
         this.tab_docente_mencion = tab_docente_mencion;
     }
 
-    public Tabla getTab_tipo_evaluacion() {
-        return tab_tipo_evaluacion;
+    public Combo getCom_periodo_academico() {
+        return com_periodo_academico;
     }
 
-    public void setTab_tipo_evaluacion(Tabla tab_tipo_evaluacion) {
-        this.tab_tipo_evaluacion = tab_tipo_evaluacion;
+    public void setCom_periodo_academico(Combo com_periodo_academico) {
+        this.com_periodo_academico = com_periodo_academico;
+    }
+
+    public Grupo getGru_pantalla() {
+        return gru_pantalla;
+    }
+
+    public void setGru_pantalla(Grupo gru_pantalla) {
+        this.gru_pantalla = gru_pantalla;
     }
 
 }
