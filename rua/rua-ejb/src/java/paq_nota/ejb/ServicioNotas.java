986f8da1661985efe7c080116f2a65bf3213c0fd
guacho20 <30877975+guacho20@users.noreply.github.com>
2018-08-10 12:55:25 -0500

    correccion notas

--- rua/rua-ejb/src/java/paq_nota/ejb/ServicioNotas.java
+++ rua/rua-ejb/src/java/paq_nota/ejb/ServicioNotas.java
@@ -39,4 +39,16 @@ public class ServicioNotas {
         sql="select ide_ynoace,descripcion_ynoace from yavirac_nota_actividad_evaluac  where activo_ynoace in ("+activo+")";
         return sql;
     }
+    
+     /**
+     * Retorna Periodo Evaluacion
+     *
+     * @param activo.- permite el ingreso del paramtero activo para filtrar ya sea true, false, o ambos.
+     * @return sql del tipo de evaluacion
+     */
+    public String getPeriodoEvaluacion(String activo) {
+        String sql="";
+        sql="select ide_ynotie,descripcion_ynotie from yavirac_nota_tipo_evaluacion  where activo_ynotie in ("+activo+")";
+        return sql;
+    }
 }
