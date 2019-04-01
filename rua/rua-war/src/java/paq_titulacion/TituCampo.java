a9fd215c8aa83ec17be1eae27068d624da0801ba
eduardosanunga <30906989+eduardosanunga@users.noreply.github.com>
2019-03-31 19:50:23 -0500

    modificado eduardo

--- rua/rua-war/src/java/paq_titulacion/TituCampo.java
+++ rua/rua-war/src/java/paq_titulacion/TituCampo.java
@@ -5,7 +5,6 @@
  */
 package paq_titulacion;
 
-
 import framework.componentes.Division;
 import framework.componentes.PanelTabla;
 import framework.componentes.Tabla;
@@ -18,76 +17,69 @@ import sistema.aplicacion.Pantalla;
  * @author ANDRES
  */
 public class TituCampo extends Pantalla {
-private Tabla tab_titu_campo = new Tabla();
-private Tabla tab_tabla = new Tabla();
+
+    private Tabla tab_titu_campo = new Tabla();
+    private Tabla tab_tabla = new Tabla();
     @EJB
 
     private final ServicioTitulacion ser_titulacion = (ServicioTitulacion) utilitario.instanciarEJB(ServicioTitulacion.class);
 
-public TituCampo (){
- tab_titu_campo.setId("tab_titu_campo");
- tab_titu_campo.setTabla("YAVIRAC_TITU_CAMPO", "IDE_YTICAM", 1);
- tab_titu_campo.setHeader("titu campo");
- tab_titu_campo.getColumna("IDE_YTICAM").setNombreVisual("CODIGO");
- tab_titu_campo.getColumna("detalle_yticam").setNombreVisual("DETALLE");
- tab_titu_campo.agregarRelacion(tab_tabla);
-  tab_titu_campo.dibujar();
- 
- 
-  PanelTabla pat_titu_campo = new PanelTabla();
+    public TituCampo() {
+        tab_titu_campo.setId("tab_titu_campo");
+        tab_titu_campo.setTabla("YAVIRAC_TITU_CAMPO", "IDE_YTICAM", 1);
+        tab_titu_campo.setHeader("titu campo");
+        tab_titu_campo.getColumna("IDE_YTICAM").setNombreVisual("CODIGO");
+        tab_titu_campo.getColumna("detalle_yticam").setNombreVisual("DETALLE");
+        tab_titu_campo.agregarRelacion(tab_tabla);
+        tab_titu_campo.dibujar();
+
+        PanelTabla pat_titu_campo = new PanelTabla();
         pat_titu_campo.setId("pat_titu_campo");
         pat_titu_campo.setPanelTabla(tab_titu_campo);
 
-
         tab_tabla.setId("tab_tabla");
- tab_tabla.setTabla("yavirac_titu_campo_linea", "ide_yticali", 2);
-tab_tabla.getColumna("ide_ytilii").setCombo(ser_titulacion.getLineaSupervision());
-  tab_tabla.dibujar();
- 
- 
-  PanelTabla pat_titu = new PanelTabla();
+        tab_tabla.setTabla("yavirac_titu_campo_linea", "ide_yticali", 2);
+        tab_tabla.getColumna("ide_ytilii").setCombo(ser_titulacion.getLineaSupervision());
+        tab_tabla.dibujar();
+
+        PanelTabla pat_titu = new PanelTabla();
         pat_titu.setId("pat_titu");
         pat_titu.setPanelTabla(tab_tabla);
 
         Division div_titu = new Division();
         div_titu.setId("div_titu");
         div_titu.dividir2(pat_titu_campo, pat_titu, "50%", "H");
-        
-        
+
         agregarComponente(div_titu);
 
-
-    
-  }
+    }
 
     @Override
     public void insertar() {
-        if(tab_titu_campo.isFocus()){
+        if (tab_titu_campo.isFocus()) {
             tab_titu_campo.insertar();
-        }
-        else if(tab_tabla.isFocus()){
+        } else if (tab_tabla.isFocus()) {
             tab_tabla.insertar();
         }
     }
 
     @Override
     public void guardar() {
-       if( tab_titu_campo.guardar()){
-           if( tab_tabla.guardar()){
+        if (tab_titu_campo.guardar()) {
+            if (tab_tabla.guardar()) {
                 guardarPantalla();
-           }
-       }
+            }
+        }
     }
 
     @Override
     public void eliminar() {
-        if(tab_titu_campo.isFocus()){
+        if (tab_titu_campo.isFocus()) {
             tab_titu_campo.eliminar();
-        }
-        else if(tab_tabla.isFocus()){
+        } else if (tab_tabla.isFocus()) {
             tab_tabla.eliminar();
         }
-        
+
     }
 
     public Tabla getTab_titu_campo() {
@@ -105,5 +97,5 @@ tab_tabla.getColumna("ide_ytilii").setCombo(ser_titulacion.getLineaSupervision()
     public void setTab_tabla(Tabla tab_tabla) {
         this.tab_tabla = tab_tabla;
     }
-    
+
 }
