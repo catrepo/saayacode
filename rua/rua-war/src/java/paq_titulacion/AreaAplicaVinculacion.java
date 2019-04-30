08c57ea5915fe1fed41bea51bec2533c823ee722
eduardosanunga <30906989+eduardosanunga@users.noreply.github.com>
2019-04-29 22:25:23 -0500

    modificado eduardo

--- rua/rua-war/src/java/paq_titulacion/AreaAplicaVinculacion.java
+++ rua/rua-war/src/java/paq_titulacion/AreaAplicaVinculacion.java
@@ -15,52 +15,52 @@ import sistema.aplicacion.Pantalla;
  * @author ANDRES
  */
 public class AreaAplicaVinculacion extends Pantalla {
-    private Tabla tab_area_aplica_vincul = new Tabla ();
+    private Tabla tab_area_aplica_vincula = new Tabla ();
     public AreaAplicaVinculacion () {
-   tab_area_aplica_vincul.setId("tab_area_aplica_vincul");
-   tab_area_aplica_vincul.setTabla("yavirac_titu_area_aplica_vincul","ide_ytiara",1);
-   tab_area_aplica_vincul.setHeader("Area Aplica Vinculacion");
-   tab_area_aplica_vincul.dibujar();
+   tab_area_aplica_vincula.setId("tab_area_aplica_vincula");
+   tab_area_aplica_vincula.setTabla("yavirac_titu_area_aplica_vincula","ide_ytiara",1);
+   tab_area_aplica_vincula.setHeader("Area Aplica Vinculacion");
+   tab_area_aplica_vincula.dibujar();
              
         
         
         PanelTabla pat_line= new PanelTabla();
         pat_line.setId("pat_line");
-        pat_line.setPanelTabla(tab_area_aplica_vincul);
+        pat_line.setPanelTabla(tab_area_aplica_vincula);
         
-        Division div_area_aplica_vincul = new Division();
-        div_area_aplica_vincul.setId("div_area_aplica_vincul");
-        div_area_aplica_vincul.dividir1(pat_line);
+        Division div_area_aplica_vincula = new Division();
+        div_area_aplica_vincula.setId("div_area_aplica_vincula");
+        div_area_aplica_vincula.dividir1(pat_line);
         
-        agregarComponente(div_area_aplica_vincul);
+        agregarComponente(div_area_aplica_vincula);
         
                
     }
 
     @Override
     public void insertar() {
-        tab_area_aplica_vincul.insertar();
+        tab_area_aplica_vincula.insertar();
 
     }
 
     @Override
     public void guardar() {
-       tab_area_aplica_vincul.guardar();
+       tab_area_aplica_vincula.guardar();
        guardarPantalla();
     }
 
     @Override
     public void eliminar() {
-        tab_area_aplica_vincul.eliminar(); 
+        tab_area_aplica_vincula.eliminar(); 
     }
     
 
-    public Tabla getTab_area_aplica_vincul() {
-        return tab_area_aplica_vincul;
+    public Tabla getTab_area_aplica_vincula() {
+        return tab_area_aplica_vincula;
     }
 
-    public void setTab_area_aplica_vincul(Tabla tab_area_aplica_vincul) {
-        this.tab_area_aplica_vincul = tab_area_aplica_vincul;
+    public void setTab_area_aplica_vincula(Tabla tab_area_aplica_vincula) {
+        this.tab_area_aplica_vincula = tab_area_aplica_vincula;
     }
     
      
