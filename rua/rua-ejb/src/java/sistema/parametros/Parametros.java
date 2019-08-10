3f1847292b263e3df431afb26e9ccd2825452988
guacho20 <30877975+guacho20@users.noreply.github.com>
2019-08-10 15:26:32 -0500

    Crear parametro para el nombre de  secretaria

--- rua/rua-ejb/src/java/sistema/parametros/Parametros.java
+++ rua/rua-ejb/src/java/sistema/parametros/Parametros.java
@@ -78,6 +78,7 @@ public class Parametros {
         lis_parametros.add(new Parametro("7", "p_tipo_exa_recuperacion", "Indica el tipo de examen de recuperacion", "2", "yavirac_nota_actividad_evaluac", "ide_ynoace", "descripcion_ynoace"));
         lis_parametros.add(new Parametro("7", "p_estado_autorizado", "Indica que esta autorizado para modificar una nota una vez cerrado el periodo academico", "2", "yavirac_nota_estado_nota", "ide_ynoest", "detalle_ynoest"));
         lis_parametros.add(new Parametro("7", "p_estado_registrado", "Indica que el docente modifico una nota que le autorizaron", "2", "yavirac_nota_estado_nota", "ide_ynoest", "detalle_ynoest"));
+        lis_parametros.add(new Parametro("7", "p_nombre_secretaria", "Indica el nombre de la secretaria para imprimir elrecord academico", "NOMBRE PRUEBA"));
         
         /*
          * TITULACION MODULO =8
